86136097a24ca1d8127f2def85ffda8b28892ce9
bubeqq <39734696+bubeqq@users.noreply.github.com>
2021-11-18 20:41:12 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,36 +1,12 @@
 #!/bin/sh
 
-# install :
-
-# curl -o installer.sh https://gist.github.com/iNem0o/6346423/raw && chmod +x installer.sh && sudo ./installer.sh
-
 sudo apt-get update
 
 sudo apt-get --no-install-recommends -y install git cmake libusb-1.0-0-dev libpulse-dev libx11-dev screen qt4-qmake libtool autoconf automake libfftw3-dev
 
 mkdir ~/src
 
-echo "Installation de rtl_sdr"
-
-cd ~/src
-
-git clone git://git.osmocom.org/rtl-sdr.git
-
-cd ~/src/rtl-sdr
-
-mkdir build
-
-cd build
-
-cmake ../ -DINSTALL_UDEV_RULES=ON
-
-make
-
-sudo make install
-
-sudo ldconfig
-
-echo "Installation de multimon-ng"
+echo "Instalacja multimon-ng z stqc"
 
 cd ~/src
 
@@ -43,24 +19,6 @@ mkdir build
 cd build
 
 qmake ../multimon-ng.pro
-
-make
-
-sudo make install
-
-echo "Installation de Kalibrate"
-
-cd ~/src
-
-git clone https://github.com/asdil12/kalibrate-rtl.git
-
-cd kalibrate-rtl
-
-git checkout arm_memory
-
-./bootstrap
-
-
 
 make
 
