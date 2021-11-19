c9f11b509aea6a5867ad3e1f72b2c3e8b1d51d33
bubeqq <39734696+bubeqq@users.noreply.github.com>
2021-11-19 18:11:47 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 
-sudo apt-get --no-install-recommends -y install git cmake libusb-1.0-0-dev libpulse-dev libx11-dev screen qt4-qmake libtool autoconf automake libfftw3-dev
+sudo apt-get --no-install-recommends -y install git cmake libusb-1.0-0-dev libpulse-dev libx11-dev screen qt4-qmake libtool autoconf automake libfftw3-dev qt4-default qt5-default qtcreator
 
 mkdir ~/src
 
@@ -10,7 +10,7 @@ echo "Instalacja multimon-ng z stqc"
 
 cd ~/src
 
-git clone https://github.com/EliasOenal/multimonNG.git
+git clone https://github.com/sq5bpf/multimon-ng-stqc.git
 
 cd ~/src/multimonNG
 
@@ -23,3 +23,5 @@ qmake ../multimon-ng.pro
 make
 
 sudo make install
+
+Echo "Zakończone! Aby uruchomić wpisz "multimon-ng -a stqc""
